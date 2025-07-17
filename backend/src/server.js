91ecd132bdf8202cd133c55b5ffdceb346efd6ee
Priyansh <priyansh2001singh@gmail.com>
2025-07-17 21:43:44 +0530

    setup and configuration of the site

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+
+const app=express();
+const PORT=process.env.PORT || 5001
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
