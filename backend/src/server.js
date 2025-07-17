eb7b27c439e8829b864807461c37a2db7ea7f33b
Priyansh <priyansh2001singh@gmail.com>
2025-07-17 23:24:46 +0530

    database configuration and stream

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config({
     path: './.env'
@@ -16,4 +17,5 @@ app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
+    connectDB();
 })
