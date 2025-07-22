bd7be74c1ed6446ac65482c3dc0e5cba82576a7e
Priyansh <priyansh2001singh@gmail.com>
2025-07-22 21:09:44 +0530

    friend request function added

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getMyFriends, getRecommendedUsers,sendFriendRequest} from "../controllers/user.controller.js"
+import {getMyFriends, getRecommendedUsers,sendFriendRequest, acceptFriendRequest} from "../controllers/user.controller.js"
 
 const router= express.Router();
 
@@ -10,5 +10,7 @@ router.get("/", getRecommendedUsers)
 router.get("/friends", getMyFriends)
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
 
 export default router
