97d40f6b45828a52265c3be39f306bc73b5eb73b
Priyansh <priyansh2001singh@gmail.com>
2025-07-23 21:01:00 +0530

    added daisyUI Toaster Tailwind and pages

---
+++ frontend/src/pages/NotificationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <div>
+      Notifiaction
+    </div>
+  )
+}
+
+export default NotificationPage
---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OnboardingPage = () => {
+  return (
+    <div>
+      ONboarding
+    </div>
+  )
+}
+
+export default OnboardingPage
